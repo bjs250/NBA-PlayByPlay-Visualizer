7f0c3aecf5e54b32e14c3140d92c707b41e257e6
Brian Schendt <brianschendt@gmail.com>
2019-03-01 21:19:50 -0500

    initial data collection script done

--- getData.py
+++ getData.py
@@ -9,12 +9,65 @@ import os
 url = "https://stats.nba.com/game/0021800922/playbyplay/"
 
 # create a new Firefox session
-driver = webdriver.Chrome()
+#driver = webdriver.Chrome()
+driver = webdriver.Firefox()
 driver.implicitly_wait(20)
 driver.get(url)
 
-soup = BeautifulSoup(driver.page_source, 'html.parser')
-print(soup.find('table'))
+visitEvents = list()
+timeEvents = list()
+scoreEvents = list()
+homeEvents = list()
 
+count = 0
+
+soup = BeautifulSoup(driver.page_source, 'html.parser')
+table = soup.find("div", class_="boxscore-pbp__inner")
+for tr in table.find_all("tr"):
+    
+    # visiting team node
+    visitNode = tr.find("td", class_="play team vtm")
+    if visitNode != None:
+        t = visitNode.find("a")
+        if t != None:
+            visitEvents.append(t.getText())
+        else:
+            visitEvents.append(None)            
+    else:
+        visitEvents.append(None)
+
+    # play status node
+    statusNode = tr.find("td", class_="play status")
+    if statusNode != None:
+        timeNode = tr.find("div", class_="time")
+        if timeNode != None:
+            timeEvents.append(timeNode.getText())
+        else:
+            timeEvents.append(None)
+
+        scoreNode = tr.find("div", class_="score")
+        if scoreNode != None:
+            scoreEvents.append(scoreNode.getText())
+        else:
+            scoreEvents.append(None)
+    else:
+        timeEvents.append(None)
+        scoreEvents.append(None)
+
+    # home team node
+    homeNode = tr.find("td", class_="play team htm")
+    if homeNode != None:
+        t = homeNode.find("a")
+        if t != None:
+            homeEvents.append(t.getText())
+        else:
+            homeEvents.append(None)            
+    else:
+        homeEvents.append(None)
+
+    count+=1
+
+df = pd.DataFrame({"time": timeEvents, "score": scoreEvents, "home": homeEvents, "visit": visitEvents})
+df.to_pickle("./stats.pkl")
 
 driver.quit()
