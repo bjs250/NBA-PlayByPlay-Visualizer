b194bf893be22781fd6f7882d0d470fb360fadff
Brian Schendt <brianschendt@gmail.com>
2019-04-05 10:33:10 -0400

    React talks to back end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,57 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    // Initialize state here
+    this.state = {
+      user_input : '',
+    };
+
+    // Bind event listening methods here
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+
+  }
+
+  // Retain and update the value of state.user_input
+  handleInputChange = (event) => {
+    this.setState({user_input: event.target.value})
+  }
+
+  // Make request to the back-end using the current state.user_input
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Current value of user input: " + this.state.user_input);
+    axios
+    .get("/api/games/")
+    .then(res => console.log(res.data))
+    .catch(err => console.log(err));
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Header</h1>
+        <form>
+          <input 
+            type='text'
+            placeholder = "Enter ID here" 
+            value={this.state.user_input}
+            onChange={this.handleInputChange}
+            />  
+          <button
+            onClick={this.handleSubmit}>
+            Submit
+          </button>
+        </form>
       </div>
     );
   }
+
 }
 
 export default App;
