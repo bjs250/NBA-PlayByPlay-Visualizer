62007152e2f5eb04b6fab04a39d1d46ab1679112
Brian Schendt <brianschendt@gmail.com>
2019-04-25 08:49:48 -0400

    BoxScore webscraping script takes command line args

--- backend/utilities/getBoxScoreData.py
+++ backend/utilities/getBoxScoreData.py
@@ -1,29 +1,46 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
-import re
 import pandas as pd
-import os
+import sys
+
+# Check if there was an input argument or not
+game_id = ""
+if len(sys.argv) == 1:
+    game_id = "0041800104"
+elif len(sys.argv) == 2:
+    game_id = str(sys.argv[1])
+else:
+    raise Exception("Wrong number of input arguments : " + str(len(sys.argv)))
 
 # launch url
-url = "https://stats.nba.com/game/0041800104/"
+url = "https://stats.nba.com/game/" + game_id + "/"
+
+# TODO: add error handling if an invalid id is specified
 
 # create a new Firefox session
 driver = webdriver.Chrome()
 #driver = webdriver.Firefox()
-driver.implicitly_wait(20)
+driver.implicitly_wait(40)
 driver.get(url)
 
 count = 0
 
-players = list()
+# Declare data structures to be used to store data for the Pandas dataframe
 headers = list()
 data = {}
 
-
 soup = BeautifulSoup(driver.page_source, 'html.parser')
+
+# Get the 2 team names
+team_nodes = soup.find_all("div", class_="nba-stat-table__caption")
+teams = []
+for team_node in team_nodes:
+    teams.append(team_node.getText())
+
 tables = soup.find_all("div", class_="nba-stat-table__overflow")
-for table in tables:
+for index,table in enumerate(tables):
+
+    # Get the box score table column headers
     for thead in table.find_all("thead"):
         for tr in thead.find_all("tr"):
             for th in tr.find_all("th"):
@@ -32,34 +49,29 @@ for table in tables:
                     if header not in headers:
                         headers.append(header)
 
-
+    # Load respective data for each player into the data dictionary
     for tr in table.find_all("tr"):
         
         # player node
         playerNode = tr.find("td", class_="player")
         if playerNode != None:
-        
-            textNode = playerNode.find("a")
-            if textNode != None:
-                playerName = textNode.getText()
-                players.append(playerName)
-                data[playerName] = {}
-            
-            # Everything else in this row is good stuff
+                    
             i = 0
             for td in tr.find_all("td"):
                 if td.getText() != None:
                     if i == 0:
-                        data[playerName] = td.getText()
+                        playerName = td.getText()
+                        if "Totals:" in playerName:
+                            playerName = teams[index]+" " +playerName
                         data[playerName] = {}
+                        data[playerName]["TEAM"] = teams[index]
                     else:
                         data[playerName][headers[i]] = td.getText()
                     i += 1
 
+# Save to file
 df = pd.DataFrame.from_dict(data).transpose()
-print(df)
+df.to_pickle("..//nba_backend//BoxScoreData//" + game_id + ".pkl")
 
-#df = pd.DataFrame({"players": players})
-#df.to_pickle("..//nba_backend//BoxScoreData//0041800104.pkl")
-
+#print(df)
 driver.quit()
