affc46ab3feb6081e7543f0e8a33b28c9aa867ed
Brian Schendt <brianschendt@gmail.com>
2019-04-29 17:02:56 -0400

    data in react-table

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -66,6 +66,7 @@ class App extends Component {
           margin={margin}>
         </Chart>
 
+        <br></br>
 
         <MyTable>
 
--- frontend/src/components/MyTable.js
+++ frontend/src/components/MyTable.js
@@ -31,42 +31,42 @@ class MyTable extends React.Component {
     {
 
       console.log("data", data)
-      console.log("keys",Object.keys(data["Unnamed: 0"]).length )
+      console.log("keys", Object.keys(data["Unnamed: 0"]).length)
 
       var formatted_data = [];
-      for (var i = 0; i < Object.keys(data["Unnamed: 0"]).length ; i++) 
-      {
-          formatted_data.push({
-            player: data["Unnamed: 0"][i],
-            reb: data["REB"][i],
-            ast: data["AST"][i]
-          })
+      for (var i = 0; i < Object.keys(data["Unnamed: 0"]).length; i++) {
+        formatted_data.push({
+          Player: data["Unnamed: 0"][i],
+          MIN: data["MIN"][i],
+          FGM: data["FGM"][i],
+          FGPercent: data["FG%"][i],
+          ThreePM: data["3PM"][i],
+          ThreePA: data["3PA"][i],
+          ThreePPercent: data["3P%"][i],
+          FTM: data["FTM"][i],
+          FTA: data["FTA"][i],
+          FTPercent: data["FT%"][i],
+          OREB: data["OREB"][i],
+          DREB: data["DREB"][i],
+          REB: data["REB"][i],
+          AST: data["AST"][i],
+          TOV: data["TOV"][i],
+          STL: data["STL"][i],
+          BLK: data["BLK"][i],
+          PF: data["PF"][i],
+          PTS: data["PTS"][i],
+          PlusMinus: data["+/-"][i]
+        })
       }
-      
-      const data2 = [{
-        player: 'Lebron James',
-        reb: "5",
-      },
-      {
-        player: 'Kevin Durant',
-        reb: "6",
-      },
-      ]
 
-      console.log(data2)
+      var headers = ['Player','MIN','FGM','FG%','3PM','3PA','3P%','FTM','FTA','FT%','OREB','DREB','REB','AST','TOV','STL','BLK','PF','PTS','+/-']
+      var accessors = ['Player','MIN','FGM','FGPercent','ThreePM','ThreePA','ThreePPercent','FTM','FTA','FTPercent','OREB','DREB','REB','AST','TOV','STL','BLK','PF','PTS','PlusMinus']
 
-      const columns = [{
-        Header: 'Player',
-        accessor: 'player'
-      }, {
-        Header: 'REB',
-        accessor: 'reb'
-      },
-      {
-        Header: 'AST',
-        accessor: 'ast'
+      var columns = []
+      for (var i = 0; i < headers.length; i++) {
+        columns.push({Header:headers[i],accessor:accessors[i]})
       }
-      ]
+      console.log(columns)
 
       return (
         <div>
