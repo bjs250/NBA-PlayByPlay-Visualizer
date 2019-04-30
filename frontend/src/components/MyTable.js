ce553e057bfed78ce640b2f9495ac5383d3236c3
Brian Schendt <brianschendt@gmail.com>
2019-04-30 08:45:01 -0400

    table broken into two tables and formatted

--- frontend/src/components/MyTable.js
+++ frontend/src/components/MyTable.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import ReactTable from 'react-table'
 import 'react-table/react-table.css'
-
-
+import '../styles/MyTable.css'
 
 class MyTable extends React.Component {
   constructor(props) {
@@ -30,50 +29,95 @@ class MyTable extends React.Component {
     if (Object.keys(data).length) // make sure data has been loaded
     {
 
-      console.log("data", data)
-      console.log("keys", Object.keys(data["Unnamed: 0"]).length)
+      // Sort the data into two tables: home and away
+      var formatted_data_home = [];
+      var formatted_data_visit = [];
+      var flag = "home"
 
-      var formatted_data = [];
       for (var i = 0; i < Object.keys(data["Unnamed: 0"]).length; i++) {
-        formatted_data.push({
-          Player: data["Unnamed: 0"][i],
-          MIN: data["MIN"][i],
-          FGM: data["FGM"][i],
-          FGPercent: data["FG%"][i],
-          ThreePM: data["3PM"][i],
-          ThreePA: data["3PA"][i],
-          ThreePPercent: data["3P%"][i],
-          FTM: data["FTM"][i],
-          FTA: data["FTA"][i],
-          FTPercent: data["FT%"][i],
-          OREB: data["OREB"][i],
-          DREB: data["DREB"][i],
-          REB: data["REB"][i],
-          AST: data["AST"][i],
-          TOV: data["TOV"][i],
-          STL: data["STL"][i],
-          BLK: data["BLK"][i],
-          PF: data["PF"][i],
-          PTS: data["PTS"][i],
-          PlusMinus: data["+/-"][i]
-        })
+        
+        if (flag === "home") {
+          formatted_data_home.push({
+            Player: data["Unnamed: 0"][i],
+            MIN: data["MIN"][i],
+            FGM: data["FGM"][i],
+            FGPercent: data["FG%"][i],
+            ThreePM: data["3PM"][i],
+            ThreePA: data["3PA"][i],
+            ThreePPercent: data["3P%"][i],
+            FTM: data["FTM"][i],
+            FTA: data["FTA"][i],
+            FTPercent: data["FT%"][i],
+            OREB: data["OREB"][i],
+            DREB: data["DREB"][i],
+            REB: data["REB"][i],
+            AST: data["AST"][i],
+            TOV: data["TOV"][i],
+            STL: data["STL"][i],
+            BLK: data["BLK"][i],
+            PF: data["PF"][i],
+            PTS: data["PTS"][i],
+            PlusMinus: data["+/-"][i]
+          }
+        )}
+        else{
+          formatted_data_visit.push({
+            Player: data["Unnamed: 0"][i],
+            MIN: data["MIN"][i],
+            FGM: data["FGM"][i],
+            FGPercent: data["FG%"][i],
+            ThreePM: data["3PM"][i],
+            ThreePA: data["3PA"][i],
+            ThreePPercent: data["3P%"][i],
+            FTM: data["FTM"][i],
+            FTA: data["FTA"][i],
+            FTPercent: data["FT%"][i],
+            OREB: data["OREB"][i],
+            DREB: data["DREB"][i],
+            REB: data["REB"][i],
+            AST: data["AST"][i],
+            TOV: data["TOV"][i],
+            STL: data["STL"][i],
+            BLK: data["BLK"][i],
+            PF: data["PF"][i],
+            PTS: data["PTS"][i],
+            PlusMinus: data["+/-"][i]
+          })
+        }
+        if (data["Unnamed: 0"][i].includes("Total")) {
+          flag = "visit"
+        }
       }
 
-      var headers = ['Player','MIN','FGM','FG%','3PM','3PA','3P%','FTM','FTA','FT%','OREB','DREB','REB','AST','TOV','STL','BLK','PF','PTS','+/-']
-      var accessors = ['Player','MIN','FGM','FGPercent','ThreePM','ThreePA','ThreePPercent','FTM','FTA','FTPercent','OREB','DREB','REB','AST','TOV','STL','BLK','PF','PTS','PlusMinus']
+      var headers = ['Player', 'MIN', 'FGM', 'FG%', '3PM', '3PA', '3P%', 'FTM', 'FTA', 'FT%', 'OREB', 'DREB', 'REB', 'AST', 'TOV', 'STL', 'BLK', 'PF', 'PTS', '+/-']
+      var accessors = ['Player', 'MIN', 'FGM', 'FGPercent', 'ThreePM', 'ThreePA', 'ThreePPercent', 'FTM', 'FTA', 'FTPercent', 'OREB', 'DREB', 'REB', 'AST', 'TOV', 'STL', 'BLK', 'PF', 'PTS', 'PlusMinus']
 
       var columns = []
       for (var i = 0; i < headers.length; i++) {
-        columns.push({Header:headers[i],accessor:accessors[i]})
+        columns.push({ Header: headers[i], accessor: accessors[i], width: 50})
       }
-      console.log(columns)
+      
+      //
+      columns[0].width = 200
+      console.log("columns",columns[0])
 
       return (
         <div>
-          <ReactTable
-            data={formatted_data}
+          <p>Home</p>
+          <ReactTable className = "test"
+            data={formatted_data_home}
             columns={columns}
+            showPagination={false}
+            defaultPageSize={formatted_data_home.length}
           />
+          <p>Visit</p>
+          <ReactTable
+            data={formatted_data_visit}
+            columns={columns}
+            showPagination={false}
+            defaultPageSize={formatted_data_visit.length}
+          />
+
         </div>
       );
 
