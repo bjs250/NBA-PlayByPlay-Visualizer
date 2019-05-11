f6051e5095756e9410a280c07dd4d64855c8e4c1
Brian Schendt <brianschendt@gmail.com>
2019-05-11 17:59:51 -0400

    database loaded

---
+++ putInDatabase.py
@@ -0,0 +1,28 @@
+import os
+import pprint
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangoBackend.settings")
+django.setup()
+from nephewStatsBackend.models import Game
+
+
+f=open("master.txt", "r")
+if f.mode == 'r':
+    contents = f.readlines()
+f.close()
+contents = [x.strip() for x in contents]
+for content in contents:
+    x = content.split(" ")
+    date = x[0]
+    game_id = x[1]
+    home = x[2]
+    away = x[3]
+    try:
+        foundGame = Game.objects.get(game_id=game_id)
+    except Game.DoesNotExist:
+        foundGame = None
+    
+    if foundGame is None:
+        game = Game(game_id=game_id,date=date,home=home,away=away)
+        game.save()
