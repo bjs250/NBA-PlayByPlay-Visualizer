d3e4e5438f5399c09b5e73645de58a8d5e6a977d
Brian Schendt <brianschendt@gmail.com>
2019-05-11 20:44:48 -0400

    deploy

--- djangoBackend/settings.py
+++ djangoBackend/settings.py
@@ -89,16 +89,16 @@ WSGI_APPLICATION = 'djangoBackend.wsgi.application'
 
 DATABASES = {  
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'nephewStatsDB',                      # Or path to database file if using sqlite3.
-        'USER': 'postgres',                      # Not used with sqlite3.
-        'PASSWORD': 'DeltaV123',                  # Not used with sqlite3.
-        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', 
+        'NAME': 'nephewStatsDB',                     
+        #'USER': 'postgres',                      # Remove in produuction
+        #'PASSWORD': 'DeltaV123',                  # Remove in production
+        'HOST': 'localhost',                      # Set to empty string for localhost. 
+        'PORT': '5432',                      # Set to empty string for default. 
     }
 }
 
-#DATABASES['default'] =  dj_database_url.config()
+DATABASES['default'] =  dj_database_url.config()
 
 
 # Password validation
