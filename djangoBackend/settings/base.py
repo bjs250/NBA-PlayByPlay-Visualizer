ef1b09eece419b1139c9ee8c992ebdf103b5e443
Brian Schendt <brianschendt@gmail.com>
2019-05-12 17:08:07 -0400

    ok

--- djangoBackend/settings/base.py
+++ djangoBackend/settings/base.py
@@ -124,8 +124,9 @@ REACT_APP_DIR = os.path.join(BASE_DIR, 'frontend')
 
 os.makedirs(STATIC_TMP, exist_ok=True)
 os.makedirs(STATIC_ROOT, exist_ok=True)
-#os.makedirs(os.path.join(REACT_APP_DIR, 'build', 'static'), exist_ok=True)
-#os.makedirs(os.path.join(REACT_APP_DIR, 'public'), exist_ok=True)
+os.makedirs(os.path.join(REACT_APP_DIR, 'public'), exist_ok=True)
+os.makedirs(os.path.join(REACT_APP_DIR, 'build', ), exist_ok=True)
+os.makedirs(os.path.join(REACT_APP_DIR, 'build', 'static'), exist_ok=True)
 
 STATICFILES_DIRS = [
     os.path.join(REACT_APP_DIR, 'build', 'static'),
