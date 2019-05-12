c864d42ef159fdf62ce311f8ce794c8e0fc48e43
Brian Schendt <brianschendt@gmail.com>
2019-05-12 17:05:28 -0400

    ok

--- djangoBackend/settings/base.py
+++ djangoBackend/settings/base.py
@@ -114,14 +114,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_TMP = os.path.join(BASE_DIR, 'static')
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 REACT_APP_DIR = os.path.join(BASE_DIR, 'frontend')
 
+os.makedirs(STATIC_TMP, exist_ok=True)
 os.makedirs(STATIC_ROOT, exist_ok=True)
-os.makedirs(os.path.join(REACT_APP_DIR, 'build', 'static'), exist_ok=True)
-os.makedirs(os.path.join(REACT_APP_DIR, 'public'), exist_ok=True)
+#os.makedirs(os.path.join(REACT_APP_DIR, 'build', 'static'), exist_ok=True)
+#os.makedirs(os.path.join(REACT_APP_DIR, 'public'), exist_ok=True)
 
 STATICFILES_DIRS = [
     os.path.join(REACT_APP_DIR, 'build', 'static'),
